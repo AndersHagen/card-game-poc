34d9ae3d61a02c94a4956201e9a546438aee8cd4
Anders Hagen <ahagen01@gmail.com>
2024-05-20 17:05:10 +0200

    Basic Turn management

---
+++ CardGame/Core/GameState/TurnManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CardGame.Core.GameState
+{
+    public class TurnManager
+    {
+        public TurnState CurrentState { get; private set; }
+
+        public int CurrentTurn { get; private set; }
+
+        public TurnManager()
+        {
+            CurrentState = TurnState.Start;
+            CurrentTurn = 1;
+        }
+
+        public void ProgressToNextState()
+        {
+            var next = ((int)CurrentState + 1) % Enum.GetValues(typeof(TurnState)).Length;
+
+            Debug.WriteLine($"- Phase-transiation '{CurrentState}' -> '{(TurnState)next}'");
+
+            CurrentState = (TurnState)next;
+        }
+    }
+}
