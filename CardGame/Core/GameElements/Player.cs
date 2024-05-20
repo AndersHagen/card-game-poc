00f94051923e7330a03d63204dfe719cf64eb93a
Anders Hagen <ahagen01@gmail.com>
2024-05-20 20:26:28 +0200

    Player class

---
+++ CardGame/Core/GameElements/Player.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame.Core.GameElements
+{
+    public class Player
+    {
+        public Deck Deck { get; set; }
+        public StackGroup Hand { get; set; }
+        public DeadPile DeadPile { get; set; }
+        public StackGroup GameArea { get; set; }
+
+        public int Mana { get; set; }
+        public int Health { get; set; }
+
+        public Player(Deck deck) 
+        {
+            GameArea = new StackGroup(new Point(350, 560), 5, 0.2f, 10, StackType.DropOnly);
+            Hand = new StackGroup(new Point(170, 800), 6, 0.25f);
+
+            Deck = deck;
+
+            DeadPile = new DeadPile(new Point(1161, 560), 0.2f);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            GameArea.Draw(spriteBatch);
+            Hand.Draw(spriteBatch);
+            DeadPile.Draw(spriteBatch);
+            Deck.Draw(spriteBatch);
+        }
+    }
+}
---
+++ CardGame/Core/GameState/Processors/DrawProcessor.cs
@@ -0,0 +1,75 @@
+﻿using CardGame.Core.GameElements;
+using CardGame.Core.GameElements.GameCards;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame.Core.GameState.Processors
+{
+    public class DrawProcessor
+    {
+        private bool _drawing;
+
+        public Card DrawnCard { get; private set; }
+        private Vector2? DrawnCardPosition;
+        private CardStack DrawnCardTarget;
+        private Vector2? DrawnCardTargetPosition;
+
+        public DrawProcessor()
+        {
+            _drawing = false;
+        }
+
+        public bool ProcessDrawFromDeck(Player player)
+        {
+            if (!_drawing)
+            {
+                return DrawFromDeck(player) == false;
+            }
+
+            if (Math.Abs((DrawnCard.Position - DrawnCardTargetPosition.Value).Length()) <= 20)
+            {
+                DrawnCardTarget.AddCard(DrawnCard);
+                DrawnCard.SetVelocity(Vector2.Zero);
+                DrawnCard = null;
+                DrawnCardPosition = null;
+                DrawnCardTarget = null;
+                DrawnCardTargetPosition = null;
+                _drawing = DrawFromDeck(player);
+
+                return _drawing == false;
+            }
+
+            return false;
+        }
+
+        private bool DrawFromDeck(Player player)
+        {
+            var target = player.Hand;
+            var deck = player.Deck;
+
+            var emptyStacks = target.GetEmptyStacks();
+
+            if (emptyStacks.Count > 0 && deck.TopCard != null)
+            {
+                var stack = emptyStacks.First();
+                var deckTopCard = deck.GetTopCard();
+
+                DrawnCard = deckTopCard;
+                DrawnCardPosition = deckTopCard.Position;
+                DrawnCardTarget = stack;
+                DrawnCardTargetPosition = stack.Bound.Center.ToVector2();
+
+                DrawnCard.SetVelocity((DrawnCardTargetPosition.Value - DrawnCardPosition.Value) / 50f);
+                _drawing = true;
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
